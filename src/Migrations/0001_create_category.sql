6a98be6c88a365bd79c917214a9c0406d01866fc
Brian Mancini <bmancini@gmail.com>
2015-05-10 18:18:25 -0400

    Added Previews SQL migrations

---
+++ src/Migrations/0001_create_category.sql
@@ -0,0 +1,23 @@
+drop table if exists category;
+create table category (
+  categoryid int primary key,
+  categoryname nvarchar(255) not null,
+  consumer tinyint default 1
+) engine=innodb;
+
+insert into category (categoryid, categoryname, consumer) values(1, 'Comics', 1);
+insert into category (categoryid, categoryname, consumer) values(2, 'Magazines', 1);
+insert into category (categoryid, categoryname, consumer) values(3, 'Graphic Novels / Trade Paperbacks', 1);
+insert into category (categoryid, categoryname, consumer) values(4, 'Books', 1);
+insert into category (categoryid, categoryname, consumer) values(5, 'Gagmes', 1);
+insert into category (categoryid, categoryname, consumer) values(6, 'Cards', 1);
+insert into category (categoryid, categoryname, consumer) values(7, 'Comic Novelties', 1);
+insert into category (categoryid, categoryname, consumer) values(8, 'Non-Comic Novelties', 1);
+insert into category (categoryid, categoryname, consumer) values(9, 'Apparel', 1);
+insert into category (categoryid, categoryname, consumer) values(10, 'Toys and Models', 1);
+insert into category (categoryid, categoryname, consumer) values(11, 'Card Supplies', 1);
+insert into category (categoryid, categoryname, consumer) values(12, 'Comic Supplies', 1);
+insert into category (categoryid, categoryname, consumer) values(13, 'Retailer Sales Tools', 0);
+insert into category (categoryid, categoryname, consumer) values(14, 'Diamond Publications', 0);
+insert into category (categoryid, categoryname, consumer) values(15, 'Posters and Printed Stuff', 1);
+insert into category (categoryid, categoryname, consumer) values(16, 'Videos and Video Games', 1);
---
+++ src/Migrations/0002_create_genre.sql
@@ -0,0 +1,46 @@
+drop table if exists genre;
+create table genre (
+  genreid int primary key auto_increment,
+  genrecode nvarchar(2) not null,
+  genrename nvarchar(255) not null
+) engine=innodb;
+
+insert into genre (genrecode, genrename) values ('AA', 'Action / Adventure');
+insert into genre (genrecode, genrename) values ('AD', 'Adult');
+insert into genre (genrecode, genrename) values ('AN', 'Anthology');
+insert into genre (genrecode, genrename) values ('AP', 'Anthropomorphics');
+insert into genre (genrecode, genrename) values ('AS', 'Art Supplies');
+insert into genre (genrecode, genrename) values ('CJ', 'Comics Journalism');
+insert into genre (genrecode, genrename) values ('CR', 'Crime');
+insert into genre (genrecode, genrename) values ('DR', 'Drama');
+insert into genre (genrecode, genrename) values ('DT', 'Designer Toys');
+insert into genre (genrecode, genrename) values ('FA', 'Fantasy');
+insert into genre (genrecode, genrename) values ('GA', 'Gaming / Role Playing');
+insert into genre (genrecode, genrename) values ('HA', 'Halloween');
+insert into genre (genrecode, genrename) values ('HO', 'Horror');
+insert into genre (genrecode, genrename) values ('HS', 'Historical');
+insert into genre (genrecode, genrename) values ('HT', 'How to Draw');
+insert into genre (genrecode, genrename) values ('HU', 'Humor/Comedy');
+insert into genre (genrecode, genrename) values ('KI', 'Kids');
+insert into genre (genrecode, genrename) values ('LG', 'Legend');
+insert into genre (genrecode, genrename) values ('LT', 'Literary');
+insert into genre (genrecode, genrename) values ('MA', 'Manga');
+insert into genre (genrecode, genrename) values ('MS', 'Mystery');
+insert into genre (genrecode, genrename) values ('MU', 'Music');
+insert into genre (genrecode, genrename) values ('MV', 'Movie/TV');
+insert into genre (genrecode, genrename) values ('PC', 'Pop Culture');
+insert into genre (genrecode, genrename) values ('PK', 'Pokémon');
+insert into genre (genrecode, genrename) values ('RB', 'Reality-Based');
+insert into genre (genrecode, genrename) values ('RF', 'Reference/Art Books/How To');
+insert into genre (genrecode, genrename) values ('RL', 'Religious');
+insert into genre (genrecode, genrename) values ('RO', 'Romance');
+insert into genre (genrecode, genrename) values ('SF', 'Science Fiction');
+insert into genre (genrecode, genrename) values ('SH', 'Super-Hero');
+insert into genre (genrecode, genrename) values ('SN', 'Seasonal');
+insert into genre (genrecode, genrename) values ('SP', 'Sports');
+insert into genre (genrecode, genrename) values ('SU', 'Surreal/Non-Linear');
+insert into genre (genrecode, genrename) values ('TY', 'Toy');
+insert into genre (genrecode, genrename) values ('WR', 'War');
+insert into genre (genrecode, genrename) values ('WS', 'Western');
+insert into genre (genrecode, genrename) values ('XX', 'Not Specieid');
+insert into genre (genrecode, genrename) values ('YA', 'Yaoi');
